65662f25f7d1fd5b2400a2774850b4c50b58a162
scyrety <mickeymed56@gmail.com>
2022-12-17 16:11:28 +0000

    Sms sending functionality

---
+++ sms.service.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+// Setting up dot env
+// composer require vlucas/phpdotenv
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+use Dotenv\Dotenv;
+use Twilio\Rest\Client;
+
+// Retrive env variable
+$sid = $_ENV['sid'];
+$token = $_ENV['token'];
+
+
+
+function sendSMS($body, $number) {
+  global $sid, $token;
+  $twilio = new Client($sid, $token);
+  try {
+    $message = $twilio->messages
+      ->create(
+        $number, // to
+        [
+          "body" => $body,
+          "from" => "+12067598669"
+        ]
+      );
+
+    if ($message->sid) {
+      return "success";
+    } else {
+      return "An error occurred";
+    }
+  } catch (Error $e) {
+    return 'An error occured ';
+  }
+}
+
+// sendSMS("This is dead", "+233550529015");
